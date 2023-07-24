3c5ed30e4518e9ebca46cfd0ccb1697e52fabdc5
spereira15 <105558705+spereira15@users.noreply.github.com>
2023-07-24 14:34:32 -0400

    added Comment model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
@@ -10,4 +16,18 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
+
